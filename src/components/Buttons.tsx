47fc2329f38f48a23b5fd63529480ea6d72887c8
Emanuele <emanuelecz06@gmail.com>
2025-05-05 23:59:49 +0200

    Responsiveness on high screens fixed

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -9,8 +9,8 @@ const Button = ({ background, text }: ButtonProps) => {
        <button
            style={{ background }}
            className="text-white p-2 sm:px-[15px] text-xs sm:text-[16px] md:text-lg  lg:text-xl xl:text-2xl md:px-5 xl:px-8 xl:py-3
-               tracking-wider shadow-2xl sm:p-3
-               rounded-tl-2xl rounded-br-2xl rounded-bl-[4px] rounded-tr-[4px] sm:mr-[0px] mr-[10px]"
+               tracking-wider shadow-2xl sm:p-3 sm:mr-3
+               rounded-tl-2xl rounded-br-2xl rounded-bl-[4px] rounded-tr-[4px mr-[10px]"
        >
            <a href="">{text}</a>
        </button>
--- src/components/ChefPage.tsx
+++ src/components/ChefPage.tsx
@@ -7,7 +7,7 @@ const ChefPage = () => {
   return (
     <div id='chef' 
     style={{backgroundImage:`url(${ChefBackground})`}}
-    className='w-full pb-10 bg-cover bg-center flex flex-col justify-between p-4 pt-10 xl:mb-0  sm:mb-10'>
+    className='w-full pb-10 bg-cover bg-center flex sm:flex-row flex-col justify-between p-4 pt-10 xl:mb-0  sm:mb-10'>
       <div className='sm:w-[50%] text-center'>
         <h1 className='text-2xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold font-[inter] text-[#101A24] mt-10 lg:mt-25'>Our Expects Chef</h1>
         <p className='text-xs sm:text-[15px] md:text-lg lg:text-2xl xl:text-3xl text-[#5c6574]'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus accusamus iusto, adipisci consectetur labore illum qui pariatur minima temporibus.</p>
